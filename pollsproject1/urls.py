8dc610a494ff3d1e7d8b96c9399e69055f0aa34e
kirbster1 <kirby.ammons@usma.edu>
2015-02-12 13:24:05 -0500

    fix urls

--- pollsproject1/urls.py
+++ pollsproject1/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'pollsproject1.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns('',
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
-]
+)
 
